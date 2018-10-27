adbdedba9c9991306b44c3fcf6b3f1263c6fa99c
coelhoptbr <coelhoptbr@gmail.com>
2018-10-26 21:37:40 -0300

    Adicionada autenticação para requests. Adicionado swagger para consultar catálogo de requests do serviço.

---
+++ src/main/java/com/example/demo/SwaggerConfig.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author rodrigo
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    //para consultar a documentacao desta api
+	//http://localhost:8080/swagger-ui.html
+}
